1c2d52871aaadf3df183b645f7d57c51032b9746
Konrad Lis <o.konradlis@gmail.com>
2020-05-11 22:09:10 +0200

    Change heading and footer in JS

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const name = "Konrad";
 console.log(
   `Cześć mam na imię ${name}! Ty też chesz wkroczyć na drogę fontendowca ?`
 );
+
+const jsText = document.querySelector(".footer__text--js");
+console.log("jstext", jsText);
+jsText.innerHTML = "Strona stworzona w ramach kursu WTF Maćka Korsna.";
+
+const headerTitle = document.querySelector(".header__title");
+console.log(headerTitle)
+
+headerTitle.innerText = "Ścieżka Frontendowca"
